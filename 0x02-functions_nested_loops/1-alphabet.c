2a8ff548b391a7a399b3715a3ec5b6ba52b73fe7
Ayomishn <ayomishn@gmail.com>
2023-06-22 05:05:45 -0700

    void print_alphabet(void);

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,13 +5,13 @@
  *
  * Return: Always 0 (success)
  */
-
-void print_alphabet(void)
+void print_alphabet(void);
 
 {
 
 	char c;
-	for(c = 'a'; c <= 'z'; c++)
+
+	for (c = 'a'; c <= 'z'; c++)
 	{
 		_putchar(c);
 	}
