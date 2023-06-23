1eb5c236ca542ed550288fbfd0632729ae5d27c1
Ayomishn <ayomishn@gmail.com>
2023-06-23 07:58:56 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - function that adds two integers together
+ * add takes in an two integer
+ * @a: character to be checked
+ * @b: character to be checked
+ * Return: a + b result
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
